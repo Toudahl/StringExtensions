06220b8888da60c846bc6343262ac14a57f59f89
Morten Toudahl <toudahl@protonmail.ch>
2019-10-05 12:53:58 +0200

    Add ValueString

---
+++ StringExtensions.Tests/ValueStringTests.cs
@@ -0,0 +1,410 @@
+﻿using FluentAssertions;
+using Morts.StringExtensions;
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using Xunit;
+
+namespace StringExtensions.Tests
+{
+    public class ValueStringTests
+    {
+        [Fact]
+        [Trait("Category", "Unit")]
+        public void ValueString_InputIsEmpty_ShouldThrow()
+        {
+            //ARRANGE
+            var input = string.Empty;
+            //ACT
+            Action act = () => new ValueString(input);
+
+            //ASSERT
+            act.Should()
+                .ThrowExactly<ArgumentException>()
+                .And
+                .Message
+                .Should()
+                .Contain("empty");
+        }
+
+        [Fact]
+        [Trait("Category", "Unit")]
+        public void ValueString_InputIsNull_ShouldThrow()
+        {
+            //ARRANGE
+            var input = (string)null;
+
+            //ACT
+            Action act = () => new ValueString(input);
+
+            //ASSERT
+            act.Should()
+                .ThrowExactly<ArgumentNullException>()
+                .And
+                .Message
+                .Should()
+                .Contain("null");
+        }
+
+        [Fact]
+        [Trait("Category", "Unit")]
+        public void ValueString_InputIsWhitespace_ShouldThrow()
+        {
+            //ARRANGE
+            var input = "   ";
+
+            //ACT
+            Action act = () => new ValueString(input);
+
+            //ASSERT
+            act.Should()
+                .ThrowExactly<ArgumentException>()
+                .And
+                .Message
+                .Should()
+                .Contain("whitespace");
+        }
+
+        [Fact]
+        [Trait("Category", "Unit")]
+        public void ValueString_InputIsNotNullEmptyOrWhitespace_ShouldNotThrow()
+        {
+            //ARRANGE
+            var input = "Morts.StringExtensions";
+
+            //ACT
+            Action act = () => new ValueString(input);
+
+            //ASSERT
+            act.Should()
+                .NotThrow();
+        }
+
+        [Fact]
+        [Trait("Category", "Unit")]
+        public void Equal_ComparedToString_ShouldBeEqual()
+        {
+            //ARRANGE
+            var input = "Morts.StringExtensions";
+            var valueString = new ValueString(input);
+
+            //ACT
+
+            var result = valueString.Equals(input);
+
+            //ASSERT
+            result.Should().BeTrue();
+        }
+
+        [Theory]
+        [Trait("Category", "Unit")]
+        [InlineData(StringComparison.Ordinal)]
+        [InlineData(StringComparison.OrdinalIgnoreCase)]
+        [InlineData(StringComparison.CurrentCulture)]
+        [InlineData(StringComparison.CurrentCultureIgnoreCase)]
+        [InlineData(StringComparison.InvariantCulture)]
+        [InlineData(StringComparison.InvariantCultureIgnoreCase)]
+        public void Equal_ComparedToStringWithStringComparison_ShouldBeEqual(StringComparison comparison)
+        {
+            //ARRANGE
+            var input = "Morts.StringExtensions";
+            var valueString = new ValueString(input);
+
+            //ACT
+
+            var result = valueString.Equals(input, comparison);
+
+            //ASSERT
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        [Trait("Category", "Unit")]
+        public void Equal_ComparedToString_ShouldNotBeEqual()
+        {
+            //ARRANGE
+            var input = "Morts.StringExtensions";
+            var valueString = new ValueString(input + "123");
+
+            //ACT
+
+            var result = valueString.Equals(input);
+
+            //ASSERT
+            result.Should().BeFalse();
+        }
+
+        [Theory]
+        [Trait("Category", "Unit")]
+        [InlineData(StringComparison.Ordinal)]
+        [InlineData(StringComparison.OrdinalIgnoreCase)]
+        [InlineData(StringComparison.CurrentCulture)]
+        [InlineData(StringComparison.CurrentCultureIgnoreCase)]
+        [InlineData(StringComparison.InvariantCulture)]
+        [InlineData(StringComparison.InvariantCultureIgnoreCase)]
+        public void Equal_ComparedToStringWithStringComparison_ShouldNotBeEqual(StringComparison comparison)
+        {
+            //ARRANGE
+            var input = "Morts.StringExtensions";
+            var valueString = new ValueString(input + "123");
+
+            //ACT
+
+            var result = valueString.Equals(input, comparison);
+
+            //ASSERT
+            result.Should().BeFalse();
+        }
+
+        [Fact]
+        [Trait("Category", "Unit")]
+        public void Equal_ComparedToValueString_ShouldBeEqual()
+        {
+            //ARRANGE
+            var input = "Morts.StringExtensions";
+            var valueString = new ValueString(input);
+            var otherValueString = new ValueString(input);
+
+            //ACT
+
+            var result = valueString.Equals(otherValueString);
+
+            //ASSERT
+            result.Should().BeTrue();
+        }
+
+        [Theory]
+        [Trait("Category", "Unit")]
+        [InlineData(StringComparison.Ordinal)]
+        [InlineData(StringComparison.OrdinalIgnoreCase)]
+        [InlineData(StringComparison.CurrentCulture)]
+        [InlineData(StringComparison.CurrentCultureIgnoreCase)]
+        [InlineData(StringComparison.InvariantCulture)]
+        [InlineData(StringComparison.InvariantCultureIgnoreCase)]
+        public void Equal_ComparedToValueStringWithStringComparison_ShouldBeEqual(StringComparison comparison)
+        {
+            //ARRANGE
+            var input = "Morts.StringExtensions";
+            var valueString = new ValueString(input);
+            var otherValueString = new ValueString(input);
+
+            //ACT
+
+            var result = valueString.Equals(otherValueString, comparison);
+
+            //ASSERT
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        [Trait("Category", "Unit")]
+        public void Equal_ComparedToValueString_ShouldBeNotEqual()
+        {
+            //ARRANGE
+            var input = "Morts.StringExtensions";
+            var valueString = new ValueString(input);
+            var otherValueString = new ValueString(input + "123");
+
+            //ACT
+
+            var result = valueString.Equals(otherValueString);
+
+            //ASSERT
+            result.Should().BeFalse();
+        }
+
+        [Theory]
+        [Trait("Category", "Unit")]
+        [InlineData(StringComparison.Ordinal)]
+        [InlineData(StringComparison.OrdinalIgnoreCase)]
+        [InlineData(StringComparison.CurrentCulture)]
+        [InlineData(StringComparison.CurrentCultureIgnoreCase)]
+        [InlineData(StringComparison.InvariantCulture)]
+        [InlineData(StringComparison.InvariantCultureIgnoreCase)]
+        public void Equal_ComparedToValueStringWithStringComparison_ShouldNotBeEqual(StringComparison comparison)
+        {
+            //ARRANGE
+            var input = "Morts.StringExtensions";
+            var valueString = new ValueString(input);
+            var otherValueString = new ValueString(input + "123");
+
+            //ACT
+
+            var result = valueString.Equals(otherValueString, comparison);
+
+            //ASSERT
+            result.Should().BeFalse();
+        }
+
+        [Fact]
+        [Trait("Category", "Unit")]
+        public void Equal_ComparedToValueStringAsObject_ShouldBeEqual()
+        {
+            //ARRANGE
+            var input = "Morts.StringExtensions";
+            var valueString = new ValueString(input);
+            var otherObject = (object)new ValueString(input);
+
+            //ACT
+
+            var result = valueString.Equals(otherObject);
+
+            //ASSERT
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        [Trait("Category", "Unit")]
+        public void Equal_ComparedToStringAsObject_ShouldBeEqual()
+        {
+            //ARRANGE
+            var input = "Morts.StringExtensions";
+            var valueString = new ValueString(input);
+
+            //ACT
+
+            var result = valueString.Equals((object)input);
+
+            //ASSERT
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        [Trait("Category", "Unit")]
+        public void Equal_ComparedToValueStringAsObject_ShouldNotBeEqual()
+        {
+            //ARRANGE
+            var input = "Morts.StringExtensions";
+            var valueString = new ValueString(input);
+            var otherObject = (object)new ValueString(input + "123");
+
+            //ACT
+
+            var result = valueString.Equals(otherObject);
+
+            //ASSERT
+            result.Should().BeFalse();
+        }
+
+        [Fact]
+        [Trait("Category", "Unit")]
+        public void Equal_ComparedToNullObject_ShouldNotBeEqual()
+        {
+            //ARRANGE
+            var input = "Morts.StringExtensions";
+            var valueString = new ValueString(input);
+            var otherObject = (string)null;
+
+            //ACT
+
+            var result = valueString.Equals(otherObject);
+
+            //ASSERT
+            result.Should().BeFalse();
+        }
+
+        [Fact]
+        [Trait("Category", "Unit")]
+        public void GetHashCode_ComparedToString_ShouldBeEqual()
+        {
+            //ARRANGE
+            var input = "Morts.StringExtensions";
+            var valueString = new ValueString(input);
+
+            //ACT
+
+            var result = valueString.GetHashCode();
+
+            //ASSERT
+            input.GetHashCode().Should().Be(result);
+        }
+
+        [Fact]
+        [Trait("Category", "Unit")]
+        public void GetHashCode_ComparedToString_ShouldNotBeEqual()
+        {
+            //ARRANGE
+            var input = "Morts.StringExtensions";
+            var valueString = new ValueString(input + "123");
+
+            //ACT
+
+            var result = valueString.GetHashCode();
+
+            //ASSERT
+            input.GetHashCode().Should().NotBe(result);
+        }
+
+        [Fact]
+        [Trait("Category", "Unit")]
+        public void ToString_StringsAreTheSame_ShouldBeEqual()
+        {
+            //ARRANGE
+            var input = "Morts.StringExtensions";
+            var valueString = new ValueString(input);
+
+            //ACT
+
+            var result = valueString.ToString();
+
+            //ASSERT
+            result.Should().Be(input);
+        }
+
+        [Fact]
+        [Trait("Category", "Unit")]
+        public void ToString_StringsAreNotTheSame_ShouldNotBeEqual()
+        {
+            //ARRANGE
+            var input = "Morts.StringExtensions";
+            var valueString = new ValueString(input + "123");
+
+            //ACT
+
+            var result = valueString.ToString();
+
+            //ASSERT
+            result.Should().NotBe(input);
+        }
+
+        [Fact]
+        [Trait("Category", "Unit")]
+        public void Implicit_StringsAreTheSame_ShouldBeEqual()
+        {
+            //ARRANGE
+            var input = "Morts.StringExtensions";
+            var valueString = new ValueString(input);
+
+            //ACT
+
+            var result = GetString(valueString);
+
+            //ASSERT
+            result.Should().Be(input);
+
+            static string GetString(ValueString value) => value;
+        }
+
+        [Fact]
+        [Trait("Category", "Unit")]
+        public void Implicit_StringsAreNotTheSame_ShouldNotBeEqual()
+        {
+            //ARRANGE
+            var input = "Morts.StringExtensions";
+            var expected = new ValueString(input);
+
+            //ACT
+
+            var result = GetValueString(input);
+
+            //ASSERT
+            result.Should().BeEquivalentTo(expected);
+
+            static ValueString GetValueString(string value) => value;
+        }
+
+
+    }
+}
---
+++ StringExtensions/Models/ValueString.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace Morts.StringExtensions
+{
+    /// <summary>
+    /// Represents a string that cannot be whitespace only, and must contain at least one char.
+    /// </summary>
+    public readonly struct ValueString : IEquatable<ValueString>, IEquatable<string>
+    {
+        private readonly string _value;
+
+        /// <summary>
+        /// Use the implicit operator instead.
+        /// </summary>
+        /// <param name="value"></param>
+        public ValueString(string value)
+        {
+            _value = value.ReturnOrThrowIfNullEmptyOrWhitespace(nameof(value));
+        }
+
+        /// <summary>
+        /// Converts the <see cref="ValueString"/> to a <see cref="string"/>
+        /// </summary>
+        /// <param name="value"></param>
+        public static implicit operator string(ValueString value) => value.ToString();
+
+        /// <summary>
+        /// Converts the <see cref="string"/> to a <see cref="ValueString"/>
+        /// </summary>
+        /// <param name="value"></param>
+        public static implicit operator ValueString(string value) => new ValueString(value);
+
+        /// <summary>
+        /// Returns the value of the encapsulated <see cref="string"/>
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return _value;
+        }
+
+        /// <summary>
+        /// Compare to <see cref="ValueString"/>
+        /// </summary>
+        public bool Equals(ValueString other) => Equals(other._value);
+
+        /// <summary>
+        /// Compare to <see cref="ValueString"/> using a <see cref="StringComparison"/>
+        /// </summary>
+        public bool Equals(ValueString other, StringComparison comparison) => Equals(other._value, comparison);
+
+        /// <summary>
+        /// Compare to <see cref="string"/>
+        /// </summary>
+        public bool Equals(string other) => _value.Equals(other);
+
+        /// <summary>
+        /// Compare to <see cref="string"/> using a <see cref="StringComparison"/>
+        /// </summary>
+        public bool Equals(string other, StringComparison comparison) => _value.Equals(other, comparison);
+
+        /// <summary>
+        /// Compare to <see cref="object"/>
+        /// </summary>
+        public override bool Equals(object other) => other is ValueString o ? Equals(o._value) : _value.Equals(other);
+
+        /// <summary>
+        /// Gets the hashcode from the encapsulated string
+        /// </summary>
+        /// <returns></returns>
+        public override int GetHashCode() => _value.GetHashCode();
+    }
+}
